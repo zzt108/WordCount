8aa121e68db481718711afdb4b599037351acdf2
zzt108 <zzt@outlook.hu>
2019-06-07 14:00:01 +0200

    Initial

---
+++ WordCount/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    class Program
+    {
+        private static IEnumerable<string> GetText(string filePath)
+        {
+            return File.ReadAllLines(filePath);
+        }
+
+        static int Main(string[] args)
+        {
+            if (args == null || args.Count() == 0)
+            {
+                Console.WriteLine("Please specify the file name to process.");
+                return(1);
+            }
+            string filePath = args[1];
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"{filePath} doesn't exists");
+                return(2);
+            }
+            var text = GetText(filePath);
+            return (0);
+        }
+    }
+}
